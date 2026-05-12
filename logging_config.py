59f470fdf607e05eb9b9230b48c9fd5076a562c5
bkruthik <balusukruthik@gmail.com>
2026-05-12 19:01:39 +0530

    Update logging_config.py

--- logging_config.py
+++ logging_config.py
@@ -2,7 +2,9 @@ import logging
 
 def setup_logging():
     logging.basicConfig(
+        
         filename="bot.log",
+        
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(message)s"
     )
